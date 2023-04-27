56f3005ff0c0b9d5969ce84478fd9337351dee9c
inannan423 <605031236@qq.com>
2023-04-27 18:32:46 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,32 +5,9 @@ import {MdFaceRetouchingNatural} from "react-icons/md";
 import {HiFingerPrint} from "react-icons/hi"
 
 function App() {
-    // const [started, setStarted] = useState(null);
     // menu, image, interactive,face
     return (
         <div className="overflow-hidden h-full ">
-            {/*  <h1 className={'text-red-500 text-2xl'}>Example React component with <a href="https://github.com/hiukim/mind-ar-js" rel="noreferrer" target="_blank">MindAR</a></h1>*/}
-
-            {/*  <div className="control-buttons">*/}
-            {/*    {started === null && <button onClick={() => {setStarted('aframe')}}>Start AFRAME version</button>}*/}
-            {/*    {started === null && <button onClick={() => {setStarted('three')}}>Start ThreeJS version</button>}*/}
-            {/*    {started !== null && <button onClick={() => {setStarted(null)}}>Stop</button>}*/}
-            {/*  </div>*/}
-
-            {/*  {started === 'aframe' && (*/}
-            {/*    <div className="container">*/}
-            {/*      <MindARViewer/>*/}
-            {/*      <video></video>*/}
-            {/*    </div>*/}
-            {/*  )}*/}
-
-            {/*  {started === 'three' && (*/}
-            {/*    <div className="container">*/}
-            {/*      <MindARThreeViewer />*/}
-            {/*    </div>*/}
-            {/*  )}*/}
-
-
             <div className={'w-full min-h-screen bg-white flex flex-col justify-start items-center py-3'}>
                 <div
                     className={'fixed text-[300px] xl:text-[500px] font-bold text-purple-400 opacity-10 -top-64 -left-32'}>
@@ -50,9 +27,6 @@ function App() {
                     target={'_blank'}
                     rel={'noreferrer'}
                     className={'w-full flex flex-col my-1 mt-8 px-8 lg:px-48 2xl:px-96 cursor-pointer'}
-                    onClick={() => {
-                        setMode('image')
-                    }}
                 >
                     <div
                         className={'w-full h-32 overflow-hidden bg-green-100 rounded-md relative border-2 border-green-500 flex items-center px-3 py-2'}>
